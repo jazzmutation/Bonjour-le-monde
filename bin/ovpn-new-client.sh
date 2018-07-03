c3e3d08b28ece2810129b7354352b39988132515
Jazz Mutation <jazztransmutation@gmail.com>
2018-07-03 20:46:31 +1000

    Removed the bin directory

--- bin/ovpn-new-client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-OVPN_DATA="ovpn-data"
-ssh -t ubuntu@$(terraform output nat.ip) sudo docker run --volumes-from $OVPN_DATA --rm -it kylemanna/openvpn easyrsa build-client-full "${1}" nopass
